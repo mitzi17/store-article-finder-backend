2720c17906b83a446ad7ab36b19e11fa29983d7d
Mitzi17 <m_estrada17@live.com>
2021-04-29 00:36:42 -0700

    define show and create actions and strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,25 @@ class ArticlesController < ApplicationController
         render json: ArticleSerializer.new(articles)
     end
 
+    def show
+        article = Article.find(params[:id])
+        render json: ArticleSerializer.new(article)
+    end
+
+    def create
+        article = Article.new(article_params)
+        
+        if article.save
+            render json: ArticleSerializer.new(article)
+        else
+            render json: {error: "Article did not save"}
+        end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:name, :number, :price, :size, :location, :category)
+    end
+
 end
